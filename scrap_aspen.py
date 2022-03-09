6a1b84f8b9800b9d2daa33c0cdd6d3def42ec062
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-03-09 12:30:28 -0300

    Modificacion de scrap_aspen para hacer request por cURL

--- scrap_aspen.py
+++ scrap_aspen.py
@@ -5,14 +5,55 @@ from urllib.parse import quote, urlparse
 
 def get_metadata():
 
-    url1 = "https://us-b4-p-e-ft6-audio.cdn.mdstrm.com/live-audio-aw/60a2745ff943100826374a70/playlist.m3u8?listeningSessionID=619482cf81feae2a_160872_7Egke9kV__0000000yu9r&downloadSessionID=0&aid=60106eadf34de307dd720e7b&dnt=true&uid=eT2eucClmkZMH2LxwiR8NXZxIozjpkCd&sid=SQQrAi05R2wBTucbgfte12NZSyRX8Qep&pid=ZWHWKtnmXWd1peh1kHen4pLNV9b9ppVN&ref=fmaspen.com&es=us-b4-p-e-ft6-audio.cdn.mdstrm.com&ote=1637370003066&ot=tcT8ppQV2X7ydMu6j-kilQ&proto=https&pz=us&cP=128000&awCollectionId=60106eadf34de307dd720e7b&liveId=60a2745ff943100826374a70&referer=https%3A%2F%2Ffmaspen.com%2F&propertyName=mediastream-player-aspen-pie&propertyType=web-app&propertyVersion=v0.0.174"
-    url2 = "https://us-b4-p-e-cz3-audio.cdn.mdstrm.com/live-audio-aw/60a2745ff943100826374a70/playlist.m3u8?listeningSessionID=61a51cdad4832ec3_592950_iPadqkkb__0000001Xepq&downloadSessionID=0&aid=60106eadf34de307dd720e7b&dnt=true&uid=8hXrApML8fcvOXIGMkPiqVRzwJRLbUeI&sid=yiyiOQMCE3sLGyb2xxaqTh5i0CYB3GYq&pid=pAuI5Ks9qjula5UOIQCYhyBkAkEfgHvs&ref=fmaspen.com&es=us-b4-p-e-cz3-audio.cdn.mdstrm.com&ote=1638890060045&ot=-NXX7sdoUCMtIaMmhNo_Qw&proto=https&pz=us&cP=128000&awCollectionId=60106eadf34de307dd720e7b&liveId=60a2745ff943100826374a70&referer=https%3A%2F%2Ffmaspen.com%2F&propertyName=mediastream-player-aspen-pie&propertyType=web-app&propertyVersion=v0.0.174"
-    url3 = "https://us-b4-p-e-ft6-audio.cdn.mdstrm.com/live-audio-aw/60a2745ff943100826374a70/playlist.m3u8?listeningSessionID=619482cf81feae2a_160872_7Egke9kV__0000000yu9r&downloadSessionID=0&aid=60106eadf34de307dd720e7b&dnt=true&uid=eT2eucClmkZMH2LxwiR8NXZxIozjpkCd&sid=SQQrAi05R2wBTucbgfte12NZSyRX8Qep&pid=ZWHWKtnmXWd1peh1kHen4pLNV9b9ppVN&ref=fmaspen.com&es=us-b4-p-e-ft6-audio.cdn.mdstrm.com&ote=1637370003066&ot=tcT8ppQV2X7ydMu6j-kilQ&proto=https&pz=us&cP=128000&awCollectionId=60106eadf34de307dd720e7b&liveId=60a2745ff943100826374a70&referer=https%3A%2F%2Ffmaspen.com%2F&propertyName=mediastream-player-aspen-pie&propertyType=web-app&propertyVersion=v0.0.174"
-    url4 = "https://us-b4-p-e-ft6-audio.cdn.mdstrm.com/live-audio-aw/60a2745ff943100826374a70/3Zho9r52uk7-327762781-4992.aac?listeningSessionID=61a51cdad4832ec3_601605_mR1HP8iK__0000001ZNrL&downloadSessionID=0&aid=60106eadf34de307dd720e7b&dnt=true&uid=8hXrApML8fcvOXIGMkPiqVRzwJRLbUeI&sid=BN3zG3YzRWNRkngul7gDF6QRAXz1LgI4&pid=WO4UVzb6sho2mDnmgAE7FdLeJ1D8GHj0&ref=fmaspen.com&es=us-b4-p-e-ft6-audio.cdn.mdstrm.com&ote=1638898714984&ot=6gXgcRUzqp3YF2drEsmCvA&proto=https&pz=us&cP=128000&awCollectionId=60106eadf34de307dd720e7b&liveId=60a2745ff943100826374a70&referer=https%3A%2F%2Ffmaspen.com%2F&propertyName=mediastream-player-aspen-pie&propertyType=web-app&propertyVersion=v0.0.174"
-    url5 = "https://us-b4-p-e-ft6-audio.cdn.mdstrm.com/live-audio-aw/60a2745ff943100826374a70/playlist.m3u8?listeningSessionID=61a51cdad4832ec3_601605_mR1HP8iK__0000001ZNrL&downloadSessionID=0&aid=60106eadf34de307dd720e7b&dnt=true&uid=8hXrApML8fcvOXIGMkPiqVRzwJRLbUeI&sid=BN3zG3YzRWNRkngul7gDF6QRAXz1LgI4&pid=WO4UVzb6sho2mDnmgAE7FdLeJ1D8GHj0&ref=fmaspen.com&es=us-b4-p-e-ft6-audio.cdn.mdstrm.com&ote=1638898714984&ot=6gXgcRUzqp3YF2drEsmCvA&proto=https&pz=us&cP=128000&awCollectionId=60106eadf34de307dd720e7b&liveId=60a2745ff943100826374a70&referer=https%3A%2F%2Ffmaspen.com%2F&propertyName=mediastream-player-aspen-pie&propertyType=web-app&propertyVersion=v0.0.174"
-    url6 = "https://us-b4-p-e-ft6-audio.cdn.mdstrm.com/live-audio-aw/60a2745ff943100826374a70/playlist.m3u8?listeningSessionID=61a51cdad4832ec3_601605_mR1HP8iK__0000001ZNrL&downloadSessionID=0&aid=60106eadf34de307dd720e7b&dnt=true&uid=8hXrApML8fcvOXIGMkPiqVRzwJRLbUeI&sid=BN3zG3YzRWNRkngul7gDF6QRAXz1LgI4&pid=WO4UVzb6sho2mDnmgAE7FdLeJ1D8GHj0&ref=fmaspen.com&es=us-b4-p-e-ft6-audio.cdn.mdstrm.com&ote=1638898714984&ot=6gXgcRUzqp3YF2drEsmCvA&proto=https&pz=us&cP=128000&awCollectionId=60106eadf34de307dd720e7b&liveId=60a2745ff943100826374a70&referer=https%3A%2F%2Ffmaspen.com%2F&propertyName=mediastream-player-aspen-pie&propertyType=web-app&propertyVersion=v0.0.174"
-    
-    r = requests.get(url1)
+# Hacemos request como cURL usando headers y params 
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0',
+        'Accept': '*/*',
+        'Accept-Language': 'es-AR,es;q=0.8,en-US;q=0.5,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Origin': 'https://fmaspen.com',
+        'Connection': 'keep-alive',
+        'Referer': 'https://fmaspen.com/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'cross-site',
+        'TE': 'trailers',
+    }
+
+    params = (
+        ('listeningSessionID', '6221b313eb6cd2c5_462105_Tbe5koVY__0000001tr8v'),
+        ('downloadSessionID', '0'),
+        ('aid', '60106eadf34de307dd720e7b'),
+        ('dnt', 'true'),
+        ('uid', 'dZ9Td24iemmQRjrEdoHCBpVLMq8e98Kx'),
+        ('sid', 'sS6vmZ7sAvCo2zPK9gxibw3ygbHFi1n4'),
+        ('pid', 'afAnEMpJQ6LcYujVnvvYVMwq1KoF0xvf'),
+        ('ref', 'fmaspen.com'),
+        ('es', 'us-b4-p-e-zs14-audio.cdn.mdstrm.com'),
+        ('ote', '1646924194688'),
+        ('ot', 'vHj3KpAEz6RTki0lqiZNRg'),
+        ('proto', 'https'),
+        ('pz', 'us'),
+        ('cP', '128000'),
+        ('awCollectionId', '60106eadf34de307dd720e7b'),
+        ('liveId', '60a2745ff943100826374a70'),
+        ('referer', 'https://fmaspen.com/'),
+        ('propertyName', 'mediastream-player-aspen-pie'),
+        ('propertyType', 'web-app'),
+        ('propertyVersion', 'v0.0.183'),
+    )
+
+    r = requests.get('https://us-b4-p-e-zs14-audio.cdn.mdstrm.com/live-audio-aw/60a2745ff943100826374a70/playlist.m3u8', headers=headers, params=params)
+
+    # Note: original query string below. It seems impossible to parse and
+    # reproduce query strings 100% accurately so the one below is given
+    # in case the reproduced version is not "correct".
+    #response = requests.get('https://us-b4-p-e-zs14-audio.cdn.mdstrm.com/live-audio-aw/60a2745ff943100826374a70/playlist.m3u8?listeningSessionID=6221b313eb6cd2c5_462105_Tbe5koVY__0000001tr8v&downloadSessionID=0&aid=60106eadf34de307dd720e7b&dnt=true&uid=dZ9Td24iemmQRjrEdoHCBpVLMq8e98Kx&sid=sS6vmZ7sAvCo2zPK9gxibw3ygbHFi1n4&pid=afAnEMpJQ6LcYujVnvvYVMwq1KoF0xvf&ref=fmaspen.com&es=us-b4-p-e-zs14-audio.cdn.mdstrm.com&ote=1646924194688&ot=vHj3KpAEz6RTki0lqiZNRg&proto=https&pz=us&cP=128000&awCollectionId=60106eadf34de307dd720e7b&liveId=60a2745ff943100826374a70&referer=https%3A%2F%2Ffmaspen.com%2F&propertyName=mediastream-player-aspen-pie&propertyType=web-app&propertyVersion=v0.0.183', headers=headers)
+
+
+# Parseamos la request
+
     soup = bs(r.text, features="lxml")
     
 
